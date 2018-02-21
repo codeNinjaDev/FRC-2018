3df0a0b2ac81565c884f99d53e6a0eebbb6c2191
Peter Chacko <workpraytoday@gmail.com>
2018-02-20 18:45:04 -0600

    changed the drive motors to spark to VictorSP

--- src/org/usfirst/frc/team3997/robot/hardware/Ports.java
+++ src/org/usfirst/frc/team3997/robot/hardware/Ports.java
@@ -5,14 +5,10 @@ import edu.wpi.first.wpilibj.I2C.Port;
 public class Ports {
 	public static final int GEAR_POT_OFFSET                        = 0;
 	// ***************** PWM PORTS *****************
-	public static final int LEFT_DRIVE_MOTOR_A_PWM_PORT            = 0; //front left
-	public static final int LEFT_DRIVE_MOTOR_B_PWM_PORT            = 1; //back left
-	public static final int RIGHT_DRIVE_MOTOR_A_PWM_PORT           = 2; //front right
-	public static final int RIGHT_DRIVE_MOTOR_B_PWM_PORT           = 3; //back right
-	public static final int CLIMBER_MOTOR_PWM_PORT                 = 6;
-	public static final int GEAR_INTAKE_MOTOR_PWM_PORT             = 5;
-	public static final int GEAR_TILTER_MOTOR_PWM_PORT             = 4;
-	public static final int CLIMBER_LOCKER_SERVO_PWM_PORT          = 7;
+	public static final int LEFT_DRIVE_MOTOR_A_PWM_PORT            = 6; //front left
+	public static final int LEFT_DRIVE_MOTOR_B_PWM_PORT            = 7; //back left
+	public static final int RIGHT_DRIVE_MOTOR_A_PWM_PORT           = 5; //front right
+	public static final int RIGHT_DRIVE_MOTOR_B_PWM_PORT           = 4; //back right
 
 	// ***************** PDP CHANNELS *****************
 	public static final int LEFT_DRIVE_MOTOR_A_PDP_CHAN           = 12;
